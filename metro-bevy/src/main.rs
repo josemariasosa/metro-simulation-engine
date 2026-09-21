19fe0367f7a5a21f0b97812d6d4711848656975f
José Maria Sosa Resendiz <jose.skedar@gmail.com>
2026-09-21 16:09:14 -0600

    Add simulation clock

--- metro-bevy/src/main.rs
+++ metro-bevy/src/main.rs
@@ -1,3 +1,11 @@
+use metro_core::station::Network;
+
 fn main() {
-    println!("{}", metro_core::hello());
+    let mut network = Network::new();
+
+    let a = network.add_station("A");
+    let b = network.add_station("B");
+    let c = network.add_station("C");
+
+    println!("Network has {} stations.", network.station_count());
 }
